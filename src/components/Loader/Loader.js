e0d54b43106272840b7b6a839626113813b629e9
Lev Ostanin <lev.ostanin@gmail.com>
2017-12-11 22:43:32 +0300

    rc

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import './Loader.css'
 
-const blue = {
-  cy: '35',
-  cx: '70',
-  r: '30'
-}
 
 const red = {
   cy: '35',
-  cx: '90',
+  cx: '35',
   r: '30'
 }
 
 const Loader = ({ fullscreen }) => (
   <div className={fullscreen ? 'loader-fullscreen' : 'loader-simple'}>
-    <svg className='rings-container' viewBox="0 0 160 70" width="160" height="70">
-      <circle className='ring blue-back' {...blue} />
-      <circle className='ring blue-front' {...blue} />
-      <circle className='ring red-back' {...red} />
+    <svg className='rings-container' viewBox="0 0 70 70" width="70" height="70">
+      {/* <circle className='ring red-back' {...red} /> */}
       <circle className='ring red-front' {...red} />
     </svg>
   </div>
